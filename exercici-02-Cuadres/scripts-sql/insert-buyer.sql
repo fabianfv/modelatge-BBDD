edce317a6c88c3de19e3a45149b8a095fcd9f6e6
fabianfv <fabianfloresvadell@gmail.com>
2020-09-27 20:09:18 +0200

    añadido el script de inserción en la tabla Buyer

---
+++ exercici-02-Cuadres/scripts-sql/insert-buyer.sql
@@ -0,0 +1,5 @@
+INSERT INTO Buyer (dni, firstName, lastName) VALUES
+    ('74829271A','Juan','García'),
+    ('83762951B','José','Martínez'),
+    ('91827364C','Manuel','Gutierrez'),
+    ('75849302D','Pablo','Pérez'); 
