e210e5c54a9bf7c88773ee32c8488abf63b5684d
fabianfv <fabianfloresvadell@gmail.com>
2020-09-27 20:04:59 +0200

    añadido el script de inserción de datos de la tabla Painting

---
+++ exercici-02-Cuadres/scripts-sql/insert-painting.sql
@@ -0,0 +1,12 @@
+INSERT INTO Painting (price , author, idBuyer) VALUES
+    (75000,'Picasso',NULL),
+    (115000,'Van Gogh',NULL),
+    (215000,'Caravagio',NULL),
+    (275000,'da Vinci',NULL),
+    (385000,'Buonarroti',NULL),
+    (90000,'Monet',NULL),
+    (175000,'Rembrandt',NULL),
+    (200000,'Rubens',NULL),
+    (250000,'Tiziano',NULL),
+    (225000,'Velázquez',NULL),
+    (175000,'Rafael',NULL); 
