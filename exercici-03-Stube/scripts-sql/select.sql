c6583cb0f27bd41b59333bb6600bae350f60d001
fabianfv <fabianfloresvadell@gmail.com>
2020-09-28 18:06:39 +0200

    añadido el script select.sql con algunas consultas pertinentes

---
+++ exercici-03-Stube/scripts-sql/select.sql
@@ -0,0 +1,17 @@
+SELECT * FROM Video
+WHERE idUser = 1;
+
+SELECT * FROM Video
+WHERE idUser = 2;
+
+SELECT * FROM Video
+WHERE title LIKE '%mysql%';
+
+SELECT * FROM Video
+WHERE title LIKE '%workbench%';
+
+SELECT * FROM Video
+WHERE title LIKE '%phpmyadmin%';
+
+SELECT * FROM Video
+WHERE title LIKE '%xampp%'; 
