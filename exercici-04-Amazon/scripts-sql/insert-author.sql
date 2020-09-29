db468806e8900bddc046a03575a8a6cb5a3f8f95
fabianfv <fabianfloresvadell@gmail.com>
2020-09-29 18:19:54 +0200

    añadido del script de inserción de la tabla Author

---
+++ exercici-04-Amazon/scripts-sql/insert-author.sql
@@ -0,0 +1,8 @@
+INSERT INTO author (idAuthor, firstName, lastName) VALUES
+    ('1', 'Mohit', 'Takkar'),
+    ('2', 'Alex', 'Libby'),
+    ('3', 'Mariot', 'Tsioara'),
+    ('4', 'Kyle', 'Sympson'),
+    ('5', 'Carlos', 'Rojas'),
+    ('6', 'Richar Mathew', 'Stalman');
+ 
