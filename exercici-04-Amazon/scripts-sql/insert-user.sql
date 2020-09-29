feac0798382f42f8abdfef420528f51989ce28b8
fabianfv <fabianfloresvadell@gmail.com>
2020-09-29 22:26:25 +0200

    añadido el script de inserción sobre la tabla user

---
+++ exercici-04-Amazon/scripts-sql/insert-user.sql
@@ -0,0 +1,4 @@
+INSERT INTO user (idUser, username, password, email) VALUES
+    ('1', 'yoda', 'y1d4_r5l2', 'yoda.rule@mail.com'),
+    ('2', 'jedi_master', 'j2d3-/-m1st2r', 'jedi.master@mail.com');
+ 
