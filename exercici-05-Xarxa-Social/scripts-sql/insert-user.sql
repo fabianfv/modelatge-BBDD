fb21fad175a2c0e09b714f99ab611580c33d4c39
fabianfv <fabianfloresvadell@gmail.com>
2020-09-30 17:51:10 +0200

    ejercicio red social terminado

---
+++ exercici-05-Xarxa-Social/scripts-sql/insert-user.sql
@@ -0,0 +1,12 @@
+INSERT INTO user (idUser, username, email, password) VALUES
+    (1, 'goku', 'goku.rule@mail.com', '82ur4uh13fi49h4'),
+    (2, 'vegeta', 'vegeta.king@mail.com', 'kafjd094wqsdaf9fd'),
+    (3, 'krillin', 'krillin.the.best@mail.com', 'kldsa8ds88dyd9887'),
+    (4, 'Bardock', 'bardok.origin@mail.com', '8jk43ds76ds76,f+saf4'),
+    (5, 'gohan', 'gohan.son@mail.com', 'jk%j4jkn983cds'),
+    (6, 'piccolo', 'piccolo.daimao@mail.com', '3"·&%d9djm440'),
+    (7, 'bulma', 'bulma.mail.com', '%/$%·SADf$··$·45'),
+    (8, 'nappa', 'nappa@mail.com', '·)jds54109¡q'),
+    (9, 'raditz', 'raditz@mail.com', '0kO4@1%2(a6k)'),
+    (10, 'selypar', 'selypar@mail.com', 'º7Jªaph0%12');
+ 
